a4861303010f38dfea66ac30e7d5d9fc458049b4
Mihail Cornescu <cornescu_mihail@yahoo.com>
2023-12-23 19:19:09 +0200

    Customer integration tests

--- spring-boot-example/src/main/java/com/mihailcornescu/customer/Customer.java
+++ spring-boot-example/src/main/java/com/mihailcornescu/customer/Customer.java
@@ -53,6 +53,10 @@ public class Customer {
         return id;
     }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public String getName() {
         return name;
     }
---
+++ spring-boot-example/src/test/java/com/mihailcornescu/journey/CustomerIntegrationTest.java
@@ -0,0 +1,141 @@
+package com.mihailcornescu.journey;
+
+import com.mihailcornescu.customer.Customer;
+import com.mihailcornescu.customer.CustomerRegistrationRequest;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class CustomerIntegrationTest {
+
+    @Autowired
+    private WebTestClient webTestClient;
+    private static final String CUSTOMER_PATH = "api/v1/customer";
+
+    @Test
+    void canRegisterCustomer() {
+        //create registration request
+        CustomerRegistrationRequest request = new CustomerRegistrationRequest(
+                "Alex",
+                "alex@mail.com",
+                19
+        );
+        //send post request
+        webTestClient
+                .post()
+                .uri("api/v1/customer")
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(Mono.just(request), CustomerRegistrationRequest.class)
+                .exchange()
+                .expectStatus()
+                .isOk();
+        //get all allCustomers
+        List<Customer> allCustomers = webTestClient.get()
+                .uri(CUSTOMER_PATH)
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus()
+                .isOk()
+                .expectBodyList(new ParameterizedTypeReference<Customer>() {
+                })
+                .returnResult()
+                .getResponseBody();
+
+        //make sure customer is there
+        Customer expectedCustomer = new Customer(request.name(), request.email(), request.age());
+        assertThat(allCustomers)
+                .usingRecursiveFieldByFieldElementComparatorIgnoringFields("id")
+                .contains(expectedCustomer);
+
+        long id = allCustomers.stream()
+                .filter(customer -> customer.getEmail().equals(request.email()))
+                .map(Customer::getId)
+                .findFirst()
+                .orElseThrow();
+
+        expectedCustomer.setId(id);
+
+        //get customer by id
+        Customer actualCustomer = webTestClient.get()
+                .uri(CUSTOMER_PATH + "/{id}", id)
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus()
+                .isOk()
+                .expectBody(new ParameterizedTypeReference<Customer>() {
+                })
+                .returnResult()
+                .getResponseBody();
+
+        assertThat(actualCustomer.getId()).isEqualTo(expectedCustomer.getId());
+        assertThat(actualCustomer.getName()).isEqualTo(expectedCustomer.getName());
+        assertThat(actualCustomer.getEmail()).isEqualTo(expectedCustomer.getEmail());
+        assertThat(actualCustomer.getAge()).isEqualTo(expectedCustomer.getAge());
+    }
+
+    @Test
+    void canDeleteCustomer() {
+        //create registration request
+        CustomerRegistrationRequest request = new CustomerRegistrationRequest(
+                "Mariah",
+                "mariah@mail.com",
+                21
+        );
+
+        //send post request
+        webTestClient
+                .post()
+                .uri(CUSTOMER_PATH)
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(Mono.just(request), CustomerRegistrationRequest.class)
+                .exchange()
+                .expectStatus()
+                .isOk();
+
+        //get all allCustomers
+        List<Customer> allCustomers = webTestClient.get()
+                .uri(CUSTOMER_PATH)
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus()
+                .isOk()
+                .expectBodyList(new ParameterizedTypeReference<Customer>() {
+                })
+                .returnResult()
+                .getResponseBody();
+
+        //make sure customer is there
+        long id = allCustomers.stream()
+                .filter(customer -> customer.getEmail().equals(request.email()))
+                .map(Customer::getId)
+                .findFirst()
+                .orElseThrow();
+
+        //delete customer by id
+        webTestClient.delete()
+                .uri(CUSTOMER_PATH + "/{id}", id)
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus()
+                .isOk();
+
+        //check that customer was deleted
+        webTestClient.get()
+                .uri(CUSTOMER_PATH + "/{id}", id)
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus()
+                .isNotFound();
+    }
+}
